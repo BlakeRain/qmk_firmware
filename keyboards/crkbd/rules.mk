2047e483eb7a571d9a98840c29d5507158983527
Blake Rain <blake.rain@blakerain.com>
2023-09-27 14:54:09 +0100

    Do not need NKRO twice

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -6,9 +6,3 @@ DEFAULT_FOLDER = crkbd/rev1
 
 RGBLIGHT_SUPPORTED = no
 RGB_MATRIX_SUPPORTED = no
-
-# Enable N-key rollover on all keyboards. In addition to its intended functionality, as of July
-# 2020, this is required for Chrome OS to process media keys. (It appears that Chrome OS filters out
-# key events from the second USB endpoint's consumer and system control devices unless that endpoint
-# also reports a keyboard or mouse device.)
-NKRO_ENABLE = yes
